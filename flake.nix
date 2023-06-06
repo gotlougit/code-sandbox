03454290ce59f6b4691d847e71a694887f05bd96
Saksham Mittal <gotlouemail@gmail.com>
2023-06-06 16:38:20 +0530

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,8 +1,6 @@
 {
   description = "code-sandbox flake";
 
-  inputs.nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
-
   outputs = { self, nixpkgs }:
     {
       packages.x86_64-linux.code-sandbox =
