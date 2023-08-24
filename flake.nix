8a5e003655dc46f4d4770fecd339e85ee55e4950
Saksham Mittal <gotlouemail@gmail.com>
2023-08-24 13:03:08 +0530

    fix: input URL

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   description = "app-sandboxes flake";
 
   outputs = { self, nixpkgs }:
